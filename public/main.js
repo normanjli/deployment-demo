cea6d5882aba6e0befe371dcdcbd093d81cf3ef7
Norman Li <normanjli@gmail.com>
2021-11-03 14:09:26 -0600

    changes

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const junkCreateCard=(num)=>{
     console.log(`test`)
     let listItem = document.getElementById(`listitem${num}`)
-    alert(listItem.innerHTML)
+    // alert(listItem.innerHTML)
     let card = document.getElementById(`junkCard`)
     card.innerHTML=''
     card.innerHTML = `<h3>${listItem.innerHTML}</h3><br><img src="https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg?quality=85&w=1024&h=512&crop=1"/>`
