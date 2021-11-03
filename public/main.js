0431fb48c78bf09aec49dc011979fd10adeaab40
Norman Li <normanjli@gmail.com>
2021-11-03 13:31:13 -0600

    dumb mistake fixes

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ const junkCreateCard=(num)=>{
     let listItem = document.getElementById(`listitem${num}`)
     alert(listItem.innerHTML)
     let card= document.getElementById(`junkCard`)
-    card.outerHTML = `<h3>${listItem}</h3><br><img url="https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg?quality=85&w=1024&h=512&crop=1"/>`
+    card.outerHTML = `<h3>${listItem.innerHTML}</h3><br><img url="https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg?quality=85&w=1024&h=512&crop=1"/>`
 }
 const testAlert=(num)=>{
     alert(num)
