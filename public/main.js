1c57fda3c77454e8e62c4e418a35796caa5bf644
Norman Li <normanjli@gmail.com>
2021-11-03 13:27:16 -0600

    test

--- public/main.js
+++ public/main.js
@@ -2,6 +2,8 @@ const junkCreateCard=(num)=>{
     console.log(`test`)
     let listItem = document.getElementById(`listitem${num}`)
     alert(listItem.innerHTML)
+    let card= document.getElementById(`junkCard`)
+    card.innerHTML = `<h3>${listItem}</h3><br><img url="https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg?quality=85&w=1024&h=512&crop=1"/>`
 }
 const testAlert=(num)=>{
     alert(num)
