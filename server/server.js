7bc457aa65d35ec68af3aa3a99756b40dbef3498
Norman Li <normanjli@gmail.com>
2021-11-03 09:42:37 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
-const express = require(`express`)
-const path = require(`path`)
+// const express = require(`express`)
+// const path = require(`path`)
+// const app = express()
+
+// app.get(`/`, (req,res)=>{
+//     res.sendFile(path.join(__dirname), `../index.html`)
+// })
+
+
+// const port = process.env.PORT || 4005
+
+// app.listen(port, ()=>console.log(`Server listening on ${port}`))
+
+const express = require('express')
+const path = require('path')
+
 const app = express()
 
-app.get(`/`, (req,res)=>{
-    res.sendFile(path.join(__dirname), `../index.html`)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
-
 
 const port = process.env.PORT || 4005
 
-app.listen(port, ()=>console.log(`Server listening on ${port}`))
-
+app.listen(port, () => console.log(`Server listening on ${port}`))
