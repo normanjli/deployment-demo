5ade1e183e8e21dc8fb76aa46d0e51a7de96c39b
Norman Li <normanjli@gmail.com>
2021-11-03 10:08:34 -0600

    style css

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-
+app.use(`/css`,express.static(path.join(__dirname, `public/styles.css`)))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
-
+app.get(`/css`,(req,res)=>{
+    res.sendFile(path.join(__dirname,`../public/styles.css`))
+})
 const port = process.env.PORT || 4005
 
 app.listen(port, () => console.log(`Server listening on ${port}`))
