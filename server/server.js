79f143ddb5f9ba34309074d352afcc7a3363a1c4
Norman Li <normanjli@gmail.com>
2021-11-03 12:41:18 -0600

    changes

--- server/server.js
+++ server/server.js
@@ -5,13 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
-app.get(`/css`,(req,res)=>{
-    res.sendFile(path.join(__dirname,`../public/styles.css`))
-})
-app.use(`/css`,express.static(path.join(__dirname, `public/styles.css`)))
-app.get(`/js`, (req, res) => {
-    res.sendFile(path.join(__dirname, `./public/main.js`))
-})
+
+app.use(express.static(`public`))
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => console.log(`Server listening on ${port}`))
