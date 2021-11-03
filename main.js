a6d28a3bd96bb8403932da26aa8257685dcd4864
Norman Li <normanjli@gmail.com>
2021-11-03 13:20:51 -0600

    does this work

---
+++ main.js
@@ -0,0 +1,8 @@
+const junkCreateCard=(num)=>{
+    console.log(`test`)
+    let listItem = document.getElementById(`listitem${num}`)
+    alert(listItem.innerHTML)
+}
+const testAlert=(num)=>{
+    alert(num)
+}
--- public/main.js
+++
@@ -1,5 +0,0 @@
-const junkAlert=()=>{
-    console.log(`test`)
-    let listItem = document.getElementById('listItem4')
-    alert(listItem.innerHTML)
-}
